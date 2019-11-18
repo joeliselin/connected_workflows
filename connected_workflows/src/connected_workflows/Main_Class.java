2d657cff9fc3cf3c3f547e7d7f0f3e75b867dd46
iselin.jo <iselin.jo@gmail.com>
2019-11-18 15:28:59 +0100

    das laufende Hello World

---
+++ connected_workflows/src/connected_workflows/Main_Class.java
@@ -0,0 +1,11 @@
+package connected_workflows;
+
+public class Main_Class {
+	
+	 public static void main(String args[]) 
+	 	{
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+}
